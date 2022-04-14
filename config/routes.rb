6544bc479dc57bc28ba1d33050ca6ee73a8e0137
gadaleang <gadaleang@gmail.com>
2022-04-14 10:16:32 +0300

    Am adaugat paginile home si about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'about/show'
+  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
+  
+  get "about",to: "about#despre"
 end
