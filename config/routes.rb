a31668984e8e266f8f32857f3febe5ef2675e593
gadaleang <gadaleang@gmail.com>
2022-04-14 11:23:50 +0300

    Am actualizat controllerele home si pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'about/show'
-  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "home#index"
-  
-  get "about",to: "about#despre"
+
+  get "about",to: "pages#about"
 end
