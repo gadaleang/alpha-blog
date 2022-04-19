0a4be62056e9f76aba7efeaf2b277750392cb42b
gadaleang <gadaleang@gmail.com>
2022-04-19 15:07:26 +0300

    Salavarea articolelor (deocamdata fara validare)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,18 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+
+  end
+
+  def create
+    #render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description)) 
+    #render plain: @article
+    @article.save
+    
+    #redirect_to article_path(@article) 
+    #echivalent cu
+    redirect_to @article
+  end
 end
